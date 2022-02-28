69189e904d0836cf98b8f7404903b690f10db742
he <bgyhenrietta@yahoo.com>
2022-02-28 10:35:10 +0100

    sentences

---
+++ src/main/java/sentences/SentenceTransformer.java
@@ -0,0 +1,26 @@
+package sentences;
+
+public class SentenceTransformer {
+
+    public String shortenSentence(String sentence) {
+        validateSentenceFirstCharAndLastChar(sentence);
+        String[] parts = sentence.split(" ");
+        if (parts.length < 5) {
+            return sentence;
+        } else {
+            return parts[0] + " ... " + parts[parts.length - 1];
+        }
+    }
+
+    private void validateSentenceFirstCharAndLastChar(String sentence) {
+        char firstChar = sentence.charAt(0);
+        char lastChar = sentence.charAt(sentence.length() - 1);
+        if (Character.isLowerCase(firstChar)) {
+            throw new IllegalArgumentException("Must start with capital letter!");
+        }
+        if (!(lastChar == '.' || lastChar == '!' || lastChar == '?')) {
+            throw new IllegalArgumentException("Must end with . ! or ?");
+        }
+
+    }
+}
